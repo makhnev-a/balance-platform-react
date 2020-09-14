92667a54ba18568f892285455dda843fbff9b2f1
Makhnev Andrey <andrey.makhnev@wifire.ru>
2020-09-14 23:08:03 +0300

    fix styles for inputs

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,23 +4,134 @@ export const Input = ({type}) => {
   const [hasLength, setHasLength] = useState(false);
   const onBlurHandler = (event) => setHasLength(!!event.currentTarget.value.trim().length);
 
-  const a = 2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2;
-  console.log(a);
-
   return (
-    <div className={hasLength ? 'input input--toTop' : 'input'}>
-      <input
-        className='input__input'
-        onBlur={onBlurHandler}
-        type={type || 'text'}
-        id='surname'
-      />
-      <label
-        className='input__label'
-        htmlFor="surname"
-      >
-        Фамилия
-      </label>
+    <div className='form__box'>
+      <div className={hasLength ? 'input input--toTop' : 'input'}>
+        <input
+          className='input__input'
+          onBlur={onBlurHandler}
+          type={type || 'text'}
+          id='surname'
+        />
+        <label
+          className='input__label'
+          htmlFor="surname"
+        >
+          Фамилия
+        </label>
+      </div>
+      <div className={hasLength ? 'input input--toTop' : 'input'}>
+        <input
+          className='input__input'
+          onBlur={onBlurHandler}
+          type={type || 'text'}
+          id='surname'
+        />
+        <label
+          className='input__label'
+          htmlFor="surname"
+        >
+          Имя
+        </label>
+      </div>
+      <div className={hasLength ? 'input input--toTop' : 'input'}>
+        <input
+          className='input__input'
+          onBlur={onBlurHandler}
+          type={type || 'text'}
+          id='surname'
+        />
+        <label
+          className='input__label'
+          htmlFor="surname"
+        >
+          Отчество
+        </label>
+      </div>
+      <div className={hasLength ? 'input input--toTop' : 'input'}>
+        <input
+          className='input__input'
+          onBlur={onBlurHandler}
+          type={type || 'text'}
+          id='surname'
+        />
+        <label
+          className='input__label'
+          htmlFor="surname"
+        >
+          Пол
+        </label>
+      </div>
+      <div className={hasLength ? 'input input--toTop' : 'input'}>
+        <input
+          className='input__input'
+          onBlur={onBlurHandler}
+          type={type || 'text'}
+          id='surname'
+        />
+        <label
+          className='input__label'
+          htmlFor="surname"
+        >
+          Дата рождения
+        </label>
+      </div>
+      <div className={hasLength ? 'input input--toTop' : 'input'}>
+        <input
+          className='input__input'
+          onBlur={onBlurHandler}
+          type={type || 'text'}
+          id='surname'
+        />
+        <label
+          className='input__label'
+          htmlFor="surname"
+        >
+          Мобильный телефон
+        </label>
+      </div>
+      <div className={hasLength ? 'input input--toTop' : 'input'}>
+        <input
+          className='input__input'
+          onBlur={onBlurHandler}
+          type={type || 'text'}
+          id='surname'
+        />
+        <label
+          className='input__label'
+          htmlFor="surname"
+        >
+          Email
+        </label>
+      </div>
+      <div className={hasLength ? 'input input--toTop' : 'input'}>
+        <input
+          className='input__input'
+          onBlur={onBlurHandler}
+          type={type || 'text'}
+          id='surname'
+        />
+        <label
+          className='input__label'
+          htmlFor="surname"
+        >
+          Адресс постоянной регистрации
+        </label>
+      </div>
+      <div className={hasLength ? 'input input--toTop' : 'input'}>
+        <input
+          className='input__input'
+          onBlur={onBlurHandler}
+          type={type || 'text'}
+          id='surname'
+        />
+        <label
+          className='input__label'
+          htmlFor="surname"
+        >
+          Название работодателя
+        </label>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './styles.scss';
+import './styles/styles.scss';
 import App from "./components/App/App";
 
 ReactDOM.render(
